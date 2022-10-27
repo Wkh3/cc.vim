6eee74ed07ee68cbe1c5fd61ebe1d0810da766a2
KH <59428912+Wkh3@users.noreply.github.com>
2022-10-27 18:13:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
 echo "----------------install git----------------"
-sudo apt-get install git -y
+apt-get install git -y
 
 echo "----------------install curl----------------"
-sudo apt-get install curl -y
+apt-get install curl -y
 
 echo "----------------install nvim----------------"
-sudo apt-get install neovim -y
+apt-get install neovim -y
 
 echo "----------------install ctags----------------"
-sudo apt-get install ctags -y
+apt-get install ctags -y
 
 echo "----------------install npm----------------"
-sudo apt-get install npm -y
+apt-get install npm -y
 
 echo  "----------------install coc.vim----------------"
-sudo curl -sL install-node.vercel.app/lts | bash -y 
-sudo apt-get install ccls -y 
-cd ~/.config/nvim/plugged/coc.nvim
-sudo npm install
+curl -sL install-node.vercel.app/lts | bash
+apt-get install ccls -y
+cd ~/.config/vim/plugged/coc.nvim
+npm install
 
 echo  "----------------install fzf---------------"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-sudo ~/.fzf/install -y
+~/.fzf/install -y
 
 echo "----------------install Vim-Plug----------------"
-sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 if [ ! -d ~/.config/nvim/ ]; then
-    sudo mkdir -p ~/.config/nvim/
+    mkdir -p ~/.config/nvim/
 fi
 
-sudo rm -f ~/.config/nvim/init.vim
-sudo cp init.vim ~/.config/nvim/init.vim
+rm -f ~/.config/nvim/init.vim
+cp init.vim ~/.config/nvim/init.vim
+cp coc_setting.json ~/.config/nvim
 
 source ~/.bashrc
